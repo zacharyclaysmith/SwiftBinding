20ec6525093d87779c2cd25143db87deb7b370ab
Zach Smith <Zachary.Clay.Smith@gmail.com>
2015-02-04 12:13:54 -0800

    changed BindableArray to have no qualifiers on T type.

--- AwfulBinding/BindableArray.swift
+++ AwfulBinding/BindableArray.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public class BindableArray<T:Equatable> : PBindableCollection{
+public class BindableArray<T> : PBindableCollection{
     private var _internalArray:[T]
     private var _changedListeners:Dictionary<NSObject, () -> Void>
     private var _indexChangedListeners:Dictionary<NSObject, ((indexChanged:Int) -> Void)>
